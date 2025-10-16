0624e5b3cc181a46f211c74224b2adc52dc13301
Jonathan Gong <jonathangong2005@gmail.com>
2025-10-16 16:43:30 -0400

    Initial Commit

---
+++ lexis/lexisApp.swift
@@ -0,0 +1,17 @@
+//
+//  lexisApp.swift
+//  lexis
+//
+//  Created by Jonathan Gong on 2025-10-16.
+//
+
+import SwiftUI
+
+@main
+struct lexisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lexisTests/lexisTests.swift
@@ -0,0 +1,17 @@
+//
+//  lexisTests.swift
+//  lexisTests
+//
+//  Created by Jonathan Gong on 2025-10-16.
+//
+
+import Testing
+@testable import lexis
+
+struct lexisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
